07e617ee0e9c664d2ea408cb6c3aa68f59e56bb5
kok-s0s <2694308562@qq.com>
2022-05-02 18:11:25 +0800

    feat: add AnimeCard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,36 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
+import React from 'react'
 import useSWR from 'swr'
 import fetcher from '../utils/fetcher'
 import type Anime from '../globals/types'
+
+const AnimeCard: React.FC<{ anime: Anime }> = ({ anime }) => {
+  return (
+    <div className="flex flex-col items-center">
+
+      <div className="relative w-24 h-32 sm:w-32 sm:h-40 flex-none">
+        <Image
+          src={anime.image}
+          alt={anime.name}
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          priority
+        />
+      </div>
+
+      <div className="flex-initial w-32 text-base sm:text-xl text-center border-b mt-2">
+        {anime.name}
+      </div>
+
+      <button className="border rounded-lg m-2 p-1 bg-gray-100 text-gray-800">Choose</button>
+
+    </div>
+  )
+}
 
 const Home: NextPage = () => {
   const { data, error } = useSWR('/api/animes', fetcher)
@@ -24,13 +51,11 @@ const Home: NextPage = () => {
         <title>Best-Anime</title>
       </Head>
 
-      <div className="font-mono text-2xl text-center pt-8">Which anime do you prefer？</div>
+      {data.map((curAnime: Anime) => (
+          <AnimeCard anime={curAnime} key={curAnime.id} />
+      ))}
 
-      <ul>
-        {data.map((post: Anime) => (
-          <li key={post.id}>{post.name}</li>
-        ))}
-      </ul>
+      <div className="font-mono text-xl text-center pt-8 text-green-100 sm:text-2xl">Which anime do you prefer<span className="italic text-2xl text-yellow-100 sm:text-4xl">?</span></div>
 
       <div className="p-8 flex justify-between items-center max-w-2xl flex-col md:flex-row animate-fade-in">
         <div className="h-36 w-36 bg-pink-300"></div>
