dfaee71a66453e48df47b1d7f8607c1e5c3ec4ac
kok-s0s <2694308562@qq.com>
2022-05-04 23:32:52 +0800

    feat: add meta messages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={false} attribute="class">
       <Head>
-          <link rel="icon" href="/images/icons8-son-goku-cute-color-favicons/Web/icons8-son-goku-cute-color-120.png" />
+        <meta name="title" content="Best-Anime" />
+        <meta name="description" content="Choose your favorite anime" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://vote-anime.vercel.app/" />
+        <meta property="og:title" content="Best-Anime" />
+        <meta property="og:description" content="Choose your favorite anime" />
+        <meta property="og:image" content="/images/gundam.png"/>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://vote-anime.vercel.app/" />
+        <meta property="twitter:title" content="Best-Anime" />
+        <meta property="twitter:description" content="Choose your favorite anime" />
+        <meta property="twitter:image" content="/images/gundam.png"/>
+        <link rel="icon" href="/images/icons8-son-goku-cute-color-favicons/Web/icons8-son-goku-cute-color-120.png" />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
