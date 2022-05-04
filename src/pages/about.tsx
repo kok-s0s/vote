0097fa3a1bb12207f466f31a3737d8e3c2219a2f
kok-s0s <2694308562@qq.com>
2022-05-05 03:47:20 +0800

    feat: update about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,7 +10,7 @@ import Footer from '@components/footer'
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale as string, ['common'])),
+      ...(await serverSideTranslations(locale as string, ['common', 'about'])),
       // Will be passed to the page component as props
     },
   }
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
 
 const AboutPage = () => {
   const router = useRouter()
-  const { t } = useTranslation('common')
+  const { t } = useTranslation(['common', 'about'])
 
   return (
     <div className="h-screen w-screen flex flex-col justify-between items-center relative">
@@ -28,8 +28,23 @@ const AboutPage = () => {
 
       <Github />
 
-      <h2 className="text-2xl p-4">{t('about')}</h2>
-      <p className="max-w-xl">My name is kok-s0s</p>
+      <h2 className="font-mono text-3xl sm:mt-8 p-4 border-double sm:border-b-8 border-gray-600 dark:border-gray-100">{t('about')}</h2>
+
+      <div className="max-w-xl flex flex-col items-stretch">
+        <div className="font-mono border m-6 p-1.5 sm:p-3 rounded-xl shadow-md border-2 shadow-gray-900 border-gray-600 bg-gray-600 text-gray-100 dark:bg-gray-100 dark:text-gray-800 dark:shadow-sky-300 dark:border-gray-100">
+          <div className="m-2 text-xl">{t('about:about-title')}</div>
+          <div>- {t('about:name')}</div>
+          <div>- {t('about:job')}</div>
+          <div>- {t('about:feature')}</div>
+        </div>
+
+        <div className="tracking-wide border m-6 p-1.5 sm:p-3 rounded-xl shadow-md border-2 shadow-gray-900 border-gray-600 bg-gray-600 text-gray-100 dark:bg-gray-100 dark:text-gray-800 dark:shadow-sky-300 dark:border-gray-100">
+          <div className="m-2 text-xl">{t('about:why-title')}</div>
+          <div className="my-2 indent-4">{t('about:why-1')}</div>
+          <div className="my-2 indent-4">{t('about:why-2')}</div>
+          <div className="my-2 indent-4">{t('about:why-3')}</div>
+        </div>
+      </div>
 
       <div className="w-80 flex justify-around text-xl pb-4">
         <Footer />
