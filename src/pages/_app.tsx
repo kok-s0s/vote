fe3d28e3bbee488a83ce15074319787c35b7c197
kok-s0s <2694308562@qq.com>
2022-05-04 23:05:43 +0800

    feat: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { appWithTranslation } from 'next-i18next'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={false} attribute="class">
+      <Head>
+          <link rel="icon" href="/images/icons8-son-goku-cute-color-favicons/Web/icons8-son-goku-cute-color-120.png" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   )
