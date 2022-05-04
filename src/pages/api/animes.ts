3e208d45484ae26eb4a3dba14cb7dcc02af4141c
kok-s0s <2694308562@qq.com>
2022-05-04 18:31:55 +0800

    feat: set game termination conditions

--- src/pages/api/animes.ts
+++ src/pages/api/animes.ts
@@ -4,6 +4,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const prisma = new PrismaClient()
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const body = req.body
+
   const animes = await prisma.anime.findMany({
     orderBy: [
       {
@@ -11,5 +13,9 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
       },
     ],
   })
+
+  if (body === 'len')
+    return res.json(animes.length)
+
   return res.json(animes)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,6 +54,23 @@ const AnimeCard: React.FC<Props> = ({ anime, chooseState, setChooseState }) => {
   )
 }
 
+const BestAnime: React.FC<{ anime: Anime }> = ({ anime }) => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="relative w-48 h-60 sm:w-64 sm:h-80 flex-none">
+        <Image
+          src={anime.image}
+          alt={anime.name}
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          priority
+        />
+      </div>
+    </div>
+  )
+}
+
 const Home: NextPage = () => {
   const [randomFirst, setRandomFirst] = useLocalStorage('randomFirst', {} as Anime)
   const [chooseFirst, setChooseFirst] = useState(false)
@@ -63,6 +80,27 @@ const Home: NextPage = () => {
   const router = useRouter()
   const { t } = useTranslation('common')
   const { theme, setTheme } = useTheme()
+  const [animeArr, setAnimeArr] = useLocalStorage('animeArr', [])
+  const [animesLen, setAnimesLen] = useLocalStorage('animeLen', 0)
+  const [bestAnime, setBestAnime] = useLocalStorage('bestAnime', {} as Anime)
+
+  useEffect(() => {
+    const getAnimesLen = async () => {
+      const body = 'len'
+
+      await fetch('/api/animes', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+        .then(res => res.json())
+        .then((data) => {
+          setAnimesLen(data)
+        })
+    }
+
+    getAnimesLen()
+  }, [])
 
   useEffect(() => {
     setLoading(true)
@@ -73,6 +111,7 @@ const Home: NextPage = () => {
           .then(res => res.json())
           .then((data) => {
             setRandomFirst(data)
+            setAnimeArr((prev: Array<string>) => [...prev, data.id])
             return data
           })
         const body = { first }
@@ -85,39 +124,74 @@ const Home: NextPage = () => {
           .then(res => res.json())
           .then((data) => {
             setRandomSecond(data)
+            setAnimeArr((prev: Array<string>) => [...prev, data.id])
           })
       }
       else if (chooseFirst) {
+        setBestAnime(randomFirst)
         const pendingUpgrade = randomSecond
         const changeless = randomFirst
         const body = { pendingUpgrade, changeless }
 
-        await fetch('/api/randomOne', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(body),
-        })
-          .then(res => res.json())
-          .then((data) => {
-            setRandomSecond(data)
+        while (true) {
+          if (animeArr.length === animesLen) {
+            setAnimeArr((prev: Array<string>) => [...prev, 'over'])
+            break
+          }
+
+          let flag = false
+
+          await fetch('/api/randomOne', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body),
           })
+            .then(res => res.json())
+            .then((data) => {
+              if (!animeArr.includes(data.id)) {
+                setRandomSecond(data)
+                setAnimeArr((prev: Array<string>) => [...prev, data.id])
+                flag = true
+              }
+            })
+
+          if (flag)
+            break
+        }
 
         setChooseFirst(!chooseFirst)
       }
       else if (chooseSecond) {
+        setBestAnime(randomSecond)
         const pendingUpgrade = randomFirst
         const changeless = randomSecond
         const body = { pendingUpgrade, changeless }
 
-        await fetch('/api/randomOne', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(body),
-        })
-          .then(res => res.json())
-          .then((data) => {
-            setRandomFirst(data)
+        while (true) {
+          if (animeArr.length === animesLen) {
+            setAnimeArr((prev: Array<string>) => [...prev, 'over'])
+            break
+          }
+
+          let flag = false
+
+          await fetch('/api/randomOne', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body),
           })
+            .then(res => res.json())
+            .then((data) => {
+              if (!animeArr.includes(data.id)) {
+                setRandomFirst(data)
+                setAnimeArr((prev: Array<string>) => [...prev, data.id])
+                flag = true
+              }
+            })
+
+          if (flag)
+            break
+        }
 
         setChooseSecond(!chooseSecond)
       }
@@ -151,14 +225,20 @@ const Home: NextPage = () => {
         <div className="font-mono text-xl text-center ml-4 dark:text-green-100 sm:text-2xl">{t('title')}<span className="italic text-2xl dark:text-yellow-100 sm:text-4xl">?</span></div>
       </div>
 
-      <div className="p-8 flex justify-between items-center max-w-2xl flex-col sm:flex-row animate-fade-in">
-        {randomFirst === null ? '' : <AnimeCard anime={randomFirst} chooseState={chooseFirst} setChooseState={setChooseFirst}/>}
-        <div className="p-8 italic text-xl">{t('vs')}</div>
-        {randomFirst === null ? '' : <AnimeCard anime={randomSecond} chooseState={chooseSecond} setChooseState={setChooseSecond}/>}
-      </div>
+      {animeArr.length === animesLen + 1
+        ? (
+          <BestAnime anime={bestAnime} />
+          )
+        : (
+          <div className="p-8 flex justify-between items-center max-w-2xl flex-col sm:flex-row animate-fade-in">
+            {randomFirst === null ? '' : <AnimeCard anime={randomFirst} chooseState={chooseFirst} setChooseState={setChooseFirst} />}
+            <div className="p-8 italic text-xl">{t('vs')}</div>
+            {randomFirst === null ? '' : <AnimeCard anime={randomSecond} chooseState={chooseSecond} setChooseState={setChooseSecond} />}
+          </div>
+          )}
 
       <div className="w-80 flex justify-around text-xl pb-4">
-        <Footer/>
+        <Footer />
 
         <Link href="/" locale={router.locale === 'en' ? 'zh' : 'en'}>
           <button>
